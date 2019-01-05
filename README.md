d3d74c86376af195d1deeb1098bd214524f92aba
isRyven <ryven.mt@gmail.com>
2019-01-05 15:38:15 +0200

    hello world

---
+++ README.md
@@ -0,0 +1,86 @@
+# SpectatorControls - Free camera controls
+
+## What?
+This is [ThreeJS](http://threjs.org) first person perspective camera controls.  
+This allows you to fly around the scene using conventional fps controls, like a spectator. [Demo](https://codesandbox.io/s/j73ln8762y).
+
+## Why?
+Builtin controls (and other implementations) often reset the view angles that were preset by the external `lookAt` call on the camera. __SpectatorControls__ picks up the view angles set manually or by other controls, and applies any pitch/yaw offsets to it. It doesn't use orbit object to follow, instead all view angle transformations done directly on camera, which makes it more flexible.
+
+## How?
+Use keyboard and mouse to manipulate the camera:
+```ini
+W = move forward
+A = move left
+S = move back
+D = move right
+Space = move up
+C = move down
+Shift = sprint
+MouseMove = adjust relative yaw and pitch of the camera
+```
+
+Import camera controls and initialize it.
+```js
+import * as THREE from "three";
+import SpectatorControls from "./SpectatorControls";
+
+// + scene / renderer / cemera / geometry setup
+
+const controls = new SpectatorControls(camera);
+const clock = new THREE.Clock();
+
+function update() {
+	controls.update(clock.getDelta());
+	renderer.render(scene, camera);
+	requestAnimationFrame(update);
+}
+
+// ...
+```
+
+## Configuration?
+
+You can control move speed, pitch and yaw sensitivity, camera move smoothness, key movement mappings and sprint acceleration:
+```js
+const controls = new SpectatorControls(camera, {
+	lookSpeed = 0.005,
+	moveSpeed = 50,
+	friction = 0.9,
+	keyMapping = { 32: 'UP' }, /* keyCode: action */ 
+	sprintMultiplier = 2
+});
+```
+
+## Methods?
+```js
+	const controls = new SpectatorControls(camera);
+	controls.enable(); // enable controls, adds key/mouse listeners
+	controls.disable(); // disable controls, removes key/mouse listeners
+	controls.isEnabled(); // check controls status
+	controls.dispose(); // remove key/mouse listeners
+	controls.update(delta = 1); // apply view angles and position offsets
+	controls.mapKey(keyCode, action); // map/remap key to action	
+```
+
+## Key mapping?
+```js
+const keyMapping = {
+	38: 'FORWARD', /* Up Arrow */
+	40: 'BACK', /* Down Arrow */
+	37: 'LEFT', /* Left Arrow */
+	39: 'RIGHT', /* Right Arrow */
+	// you can also map next actions:
+	// UP
+	// DOWN
+	// SPRINT
+};
+// this will add additional set of key mappings to existing ones
+// if you want to disable the existing mappings, remap key codes to null string eg. { 87: "" }, remaps W to noop
+const controls = new SpectatorControls(camera, { keyMapping });
+controls.mapKey(17, 'DOWN'); // maps CTRL to move down
+```
+
+## License
+MIT
+
---
+++ SpectatorControls.js
@@ -0,0 +1,155 @@
+import * as THREE from 'three';
+
+// actions
+const FORWARD = 1 << 0;
+const LEFT = 1 << 1;
+const RIGHT = 1 << 2;
+const BACK = 1 << 3;
+const UP = 1 << 4;
+const DOWN = 1 << 5;
+const SPRINT = 1 << 6;
+
+// defaults
+const MOVESPEED = 50;
+const FRICTION = 0.9;
+const LOOKSPEED = 0.005;
+const SPRINTMULT = 2;
+const KEYMAPPING = {
+	87: 'FORWARD', /* W */
+	83: 'BACK', /* S */
+	65: 'LEFT', /* A */
+	68: 'RIGHT', /* D */
+	32: 'UP', /* Spacebar */
+	67: 'DOWN', /* C */
+	16: 'SPRINT', /* Shift */
+};
+
+export default class SpectatorControls {
+	constructor(camera, {
+		lookSpeed = LOOKSPEED,
+		moveSpeed = MOVESPEED,
+		friction = FRICTION,
+		keyMapping = KEYMAPPING,
+		sprintMultiplier = SPRINTMULT
+	} = {}) {
+		this.camera = camera;
+		this.lookSpeed = lookSpeed;
+		this.moveSpeed = moveSpeed;
+		this.friction = friction;
+		this.sprintMultiplier = sprintMultiplier;
+		this.keyMapping = Object.assign({}, KEYMAPPING, keyMapping);
+		this.enabled = false;
+		this._mouseState = { x: 0, y: 0 };
+		this._keyState = { press: 0, prevPress: 0 };
+		this._moveState = { velocity: new THREE.Vector3(0, 0, 0) };
+		this._processMouseMoveEvent = this._processMouseMoveEvent.bind(this);
+		this._processKeyEvent = this._processKeyEvent.bind(this);
+	}
+	_processMouseMoveEvent(event) {
+		this._processMouseMove(
+			event.movementX || event.mozMovementX || event.webkitMovementX,
+			event.movementY || event.mozMovementY || event.webkitMovementY
+		);
+	}
+	_processMouseMove(x = 0, y = 0) {
+		this._mouseState = { x, y };
+	}
+	_processKeyEvent(event) {
+		this._processKey(event.keyCode, event.type === "keydown");
+	}
+	_processKey(key, isPressed) {
+		const { press } = this._keyState;
+		let newPress = press;
+		switch (this.keyMapping[key]) {
+			case 'FORWARD':
+				isPressed ? newPress |= FORWARD : newPress &= ~FORWARD;
+				break;
+			case 'BACK':
+				isPressed ? newPress |= BACK : newPress &= ~BACK;
+				break;
+			case 'LEFT':
+				isPressed ? newPress |= LEFT : newPress &= ~LEFT;
+				break;
+			case 'RIGHT':
+				isPressed ? newPress |= RIGHT : newPress &= ~RIGHT;
+				break;
+			case 'UP':
+				isPressed ? newPress |= UP : newPress &= ~UP;
+				break;
+			case 'DOWN':
+				isPressed ? newPress |= DOWN : newPress &= ~DOWN;
+				break;
+			case 'SPRINT':
+				isPressed ? newPress |= SPRINT : newPress &= ~SPRINT;
+				break;
+			default:
+				break;
+		}
+		this._keyState.press = newPress;
+	}
+	enable() {
+		document.addEventListener('mousemove', this._processMouseMoveEvent);
+		document.addEventListener('keydown', this._processKeyEvent);
+		document.addEventListener('keyup', this._processKeyEvent);
+		this.enabled = true;
+		this.camera.rotation.reorder("YXZ");
+	}
+	disable() {
+		document.removeEventListener('mousemove', this._processMouseMoveEvent);
+		document.removeEventListener('keydown', this._processKeyEvent);
+		document.removeEventListener('keyup', this._processKeyEvent);
+		this.enabled = false;
+		this._keyState.press = 0;
+		this._keyState.prevPress = 0;
+		this._mouseState = { x: 0, y: 0 };
+		this.camera.rotation.reorder("XYZ");
+	}
+	isEnabled() {
+		return this.enabled;
+	}
+	dispose() {
+		this.disable();
+	}
+	update(delta = 1) {
+		if (!this.enabled) {
+			// finish move transition
+			if (this._moveState.velocity.length() > 0) {
+				this._moveCamera(this._moveState.velocity);
+			}
+			return;
+		}
+		// view angles
+		const actualLookSpeed = delta * this.lookSpeed;
+		const lon = ((20 * this._mouseState.x) * actualLookSpeed);
+		const lat = ((20 * this._mouseState.y) * actualLookSpeed);
+		this.camera.rotation.x -= lat;
+		this.camera.rotation.y -= lon;
+		this._mouseState = { x: 0, y: 0 };
+
+		// movements
+		let actualMoveSpeed = delta * this.moveSpeed;
+		const velocity = this._moveState.velocity.clone();
+		const { press } = this._keyState;
+		if (press & SPRINT) actualMoveSpeed *= this.sprintMultiplier;
+		if (press & FORWARD) velocity.z = -actualMoveSpeed;
+		if (press & BACK) velocity.z = actualMoveSpeed;
+		if (press & LEFT) velocity.x = -actualMoveSpeed;
+		if (press & RIGHT) velocity.x = actualMoveSpeed;
+		if (press & UP) velocity.y = actualMoveSpeed;
+		if (press & DOWN) velocity.y = -actualMoveSpeed;
+		this._moveCamera(velocity);
+
+		this._moveState.velocity = velocity;
+		this._keyState.prevPress = press;
+	}
+	_moveCamera(velocity) {
+		velocity.multiplyScalar(this.friction);
+		velocity.clampLength(0, this.moveSpeed);
+		this.camera.translateZ(velocity.z);
+		this.camera.translateX(velocity.x);
+		this.camera.translateY(velocity.y);
+	}
+	mapKey(key, action) {
+		this.keyMapping = Object.assign({}, this.keyMapping, { [key]: action });
+	}
+}
